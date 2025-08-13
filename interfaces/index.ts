d2f2e7af9e4283db8bf8c56bace78bdcc7560c6c
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-13 20:58:35 +0100

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,19 @@
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
 

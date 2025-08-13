fae004ed8f8307c866c46e7ffdda82c0ff66ee84
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-13 21:00:18 +0100

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
 

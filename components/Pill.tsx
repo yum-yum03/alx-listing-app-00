b1efc1f879ee274d2cd10ec7c73607a5f715f56b
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-13 21:01:21 +0100

    Update Pill.tsx

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,1 +1,20 @@
 
+import React from "react";
+
+interface PillProps {
+  label: string;
+  onClick?: () => void;
+}
+
+const Pill: React.FC<PillProps> = ({ label, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 border border-gray-300 rounded-full text-sm hover:bg-gray-200"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Pill;

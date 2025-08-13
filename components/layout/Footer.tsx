d0d280b587315489b8e0b1f8be9e62a627c8d140
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-13 20:59:57 +0100

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-6 text-center mt-12">
+      <p>&copy; {new Date().getFullYear()} StayFinder. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
 
